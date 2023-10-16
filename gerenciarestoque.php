8c2583257e90b82307acd8f5de3c49519acf7fcb
Fabiana Vilela <fabiana.vlmoura@sp.senac.br>
2023-10-16 19:18:02 -0300

    Update gerenciarestoque.php

--- gerenciarestoque.php
+++ gerenciarestoque.php
@@ -1,5 +1,46 @@
+<?php
 
-foreach ($roupas as $isbn => $detalhes)
+$roupas = [
+    '1' => [
+        'nome' => 'Jaqueta',
+        'preco' => 55.40,
+        'qtd_estoque' => 120
+    ],
+
+    '2' => [
+        'nome' => 'Calça',
+        'preco' => 65.40,
+        'qtd_estoque' => 50
+    ],
+    '3' => [
+        'nome' => 'Regata',
+        'preco' => 66.40,
+        'qtd_estoque' => 200
+    ],
+
+    '4' => [
+        'nome' => 'Blusa',
+        'preco' => 66.89,
+        'qtd_estoque' => 65
+    ],
+    '5' => [
+        'nome' => 'Colete',
+        'preco' => 39.99,
+        'qtd_estoque' => 100
+    ],
+
+    '6' => [
+        'nome' => 'Manga longa',
+        'preco' => 65.78,
+        'qtd_estoque' => 40
+    ],
+    '7' => [
+        'nome' => 'Blazer',
+        'preco' => 189.99,
+        'qtd_estoque' => 0
+    ],
+];
+foreach ($roupas as $codigo => $detalhes)
 {
     echo "Nome: " . $detalhes['nome'] . "\n";
     echo "Quantidade em estoque: " . $detalhes['qtd_estoque'] . "\n\n";
